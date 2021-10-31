98abaed842e41c76d80b2ffb1e7b10a7cee113eb
Hadra 92i <hadraiizi547@gmail.com>
2021-10-31 16:01:11 +0000

    Mis en place du front end

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,39 @@
-<!-- Footer -->
-<footer class="bg-light text-muted">
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: #7451EB; color: #fff">
-      Hadra 92I DEV &copy; {{ date('Y') }} Copyright. All rights reserved
+<!-- Footer-->
+<footer class="border-top">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item">
+                        <a href="#!">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#!">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#!">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+                <div class="small text-center text-muted fst-italic">Copyright &copy; Laravel Blog {{date('Y')}}</div>
+            </div>
+        </div>
     </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
+</footer>
+ 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('js/scripts.js')}}"></script>
