76d0a08285502407038d5cd526c20394a63f8a61
Hadra 92i <hadraiizi547@gmail.com>
2021-10-31 00:39:01 +0000

    Creation d'une app laravel et mise en place de la gestion d'authentification

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="bg-light text-muted">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: #7451EB; color: #fff">
+      Hadra 92I DEV &copy; {{ date('Y') }} Copyright. All rights reserved
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
